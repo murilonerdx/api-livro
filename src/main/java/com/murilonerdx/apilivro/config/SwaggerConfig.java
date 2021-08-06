d49c321758b8bc4f86063124f0ef4a7f8195b988
murilonerdx <mu-silva@outlook.com>
2021-08-05 22:19:30 -0300

    Configurando Swagger

---
+++ src/main/java/com/murilonerdx/apilivro/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.murilonerdx.apilivro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.murilonerdx.apilivro.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("Projeto de controle de aluguel de livros")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact("Murilo Pereira","https://github.com/murilonerdx","mu-silva@outlook.com");
+    }
+}
