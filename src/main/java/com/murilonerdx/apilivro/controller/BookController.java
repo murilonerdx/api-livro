c387b1a24e8c64232238bfa8618a7292d750f241
murilonerdx <mu-silva@outlook.com>
2021-08-06 00:33:39 -0300

    Só commitei pra dizer o que o pato disse pra pata: Oi Pata! Vamos patinar?

--- src/main/java/com/murilonerdx/apilivro/controller/BookController.java
+++ src/main/java/com/murilonerdx/apilivro/controller/BookController.java
@@ -3,9 +3,7 @@ package com.murilonerdx.apilivro.controller;
 import com.murilonerdx.apilivro.dto.BookDTO;
 import com.murilonerdx.apilivro.dto.LoanDTO;
 import com.murilonerdx.apilivro.entity.Book;
-import com.murilonerdx.apilivro.entity.Loan;
-import com.murilonerdx.apilivro.exceptions.ApiErrors;
-import com.murilonerdx.apilivro.exceptions.BusinessException;
+
 import com.murilonerdx.apilivro.service.BookService;
 import com.murilonerdx.apilivro.service.LoanService;
 import java.util.List;
@@ -16,15 +14,14 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
